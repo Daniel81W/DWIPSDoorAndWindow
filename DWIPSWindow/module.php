8bd183adf6a2e037445549e5d4d0d6f49960dfc9
Daniel81W <kenya81@gmail.com>
2023-11-05 17:56:52 +0100

    Initial commit

--- DWIPSWindow/module.php
+++ DWIPSWindow/module.php
@@ -27,7 +27,7 @@
             $instID = 0;
             //foreach ()
             echo $MessageList;
-            $this->SendDebug("Messages", var_dump($MessageList),0);
+            //$this->SendDebug("Messages", var_dump($MessageList),0);
 
 			$this->RegisterMessage($this->ReadPropertyInteger("WindowSensor1ID"),10603);
             $this->RegisterMessage($this->ReadPropertyInteger("WindowSensor2ID"),10603);
