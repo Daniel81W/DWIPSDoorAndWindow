d21a250568e017809479e20d3ceb8e3d44cbb548
Daniel81W <kenya81@gmail.com>
2023-11-05 17:14:01 +0100

    Initial commit

---
+++ DWIPSWindow/module.php
@@ -0,0 +1,52 @@
+<?php
+	//include_once("/var/lib/symcon/modules/DWIPSLib/libs/astro.php");
+	class DWIPSWindow extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			$this->RegisterPropertyInteger("WindowSensor1ID", 0);
+            $this->RegisterPropertyInteger("WindowSensor2ID", 0);
+			
+
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			$this->RegisterMessage($this->ReadPropertyInteger("WindowSensor1ID"),10603);
+            $this->RegisterMessage($this->ReadPropertyInteger("WindowSensor2ID"),10603);
+		}
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdateSunrise($id);
+        *
+        */
+
+		public function ReceiveData($JSONString) {
+			
+		}
+
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+	
+			//IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: ".print_r($Data, true));
+
+
+		
+			
+		}
+		
+	}
+	?>
