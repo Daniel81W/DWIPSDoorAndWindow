377be2e54d56ff8951bdfd6da44a3bc363ca38bc
Daniel81W <kenya81@gmail.com>
2023-11-05 20:53:55 +0100

    Neue Variable

---
+++ DWIPSWindowControl/module.php
@@ -0,0 +1,44 @@
+<?php
+	class DWIPSWindow extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+            $this->RegisterVariableInteger('count', $this->Translate('count'), '',1);
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+
+		}
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdateSunrise($id);
+        *
+        */
+
+		public function ReceiveData($JSONString) {
+			
+		}
+
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+	
+			//IPS_LogMessage("MessageSink", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: ".print_r($Data, true));
+			
+		}
+		
+	}
+	?>
