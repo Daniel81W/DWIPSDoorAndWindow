d5627dbf5f7f9d4df753d7a57437f42fbff8f3e5
Daniel81W <kenya81@gmail.com>
2023-11-06 20:11:08 +0100

    Fensterstatus wird jetzt nach den Variablen gesetzt

--- DWIPSWindowControl/module.php
+++ DWIPSWindowControl/module.php
@@ -29,7 +29,7 @@ class DWIPSWindowControl extends IPSModule {
 			//Never delete this line!
 			parent::ApplyChanges();
 
-            /*$arrString = $this->ReadPropertyString("windows");
+            /**$arrString = $this->ReadPropertyString("windows");
             $arr = json_decode($arrString);
             $this->SendDebug( "Liste", "".$arr[0], 0);*/
 
